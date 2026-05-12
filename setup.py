b3e04aeac97f83269a98afe246384fd11015f4e6
mrkeith99 <kvalentincardenas@hotmail.com>
2026-05-12 21:13:00 +0900

    [FIX] entry points

--- setup.py
+++ setup.py
@@ -20,7 +20,11 @@ setup(
     maintainer_email="unity-robotics@unity3d.com",
     description="ROS TCP Endpoint Unity Integration (ROS2 version)",
     license="Apache 2.0",
-    tests_require=["pytest"],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         "console_scripts": [
             "default_server_endpoint = ros_tcp_endpoint.default_server_endpoint:main"
